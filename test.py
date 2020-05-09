c8d1cf32cb3b5768226d2d15b342aac0a52e7284
jiajian_wu <jaywu.quant@gmail.com>
2020-05-09 17:42:35 -0400

    Able to deal with falling collision - with class

--- test.py
+++ test.py
@@ -15,30 +15,30 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
 # Set the height and width of the screen
-size = [400, 400]
+size = [160, 200]
 screen = pygame.display.set_mode(size)
 
 
 # class to store all the sitting blocks
 class Block_List:
-    pos_s = set()  # set to store positions of the sitting blocks in "top:left" fashion.
-    bl = []  # list to hold all the blocks
+    d = {}
 
     def draw_all(self, surface):
-        for block in self.bl:
+        for block in self.d.values():
             block.draw(surface)
 
     def append(self, b):
-        self.bl.append(b)
-        self.pos_s.add(b.block.topleft)
+        self.d[b.block.topleft] = b
+
+    # def clear_row(self):
 
 
 class Block(Rect):
     def __init__(self):
-        self.block = Rect(20, 20, 20, 20)
+        self.block = Rect(0, 20, 20, 20)
 
-    def collide_down(self, s):
-        if self.block.bottomleft in s:
+    def collide_down(self, d):
+        if self.block.bottomleft in d:
             return True
         return False
 
@@ -77,11 +77,10 @@ while not done:
     screen.fill(WHITE)
 
     curr_block.key_press()
-    print(block_list.pos_s, "bottom left pos ", curr_block.block.bottomleft)
+    print(block_list.d, "bottom left pos ", curr_block.block.bottomleft)
 
-    if curr_block.block.bottom > 300 or curr_block.collide_down(block_list.pos_s):
+    if curr_block.block.bottom >= 200 or curr_block.collide_down(block_list.d):
         block_list.append(curr_block)  # add to sitting blocks then create a new block
-        print(len(block_list.bl))
         curr_block = Block()
 
     curr_block.draw(screen)  # draw the falling block
